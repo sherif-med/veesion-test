1b0aa89890ae1ece84afd8b6f554727757392ad2
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-06-23 05:49:34 +0200

    fixing prediction output for video classification task && adding argument "temporal modeling" to choose between transformers and lstm

--- src/tasks/video_classification.py
+++ src/tasks/video_classification.py
@@ -1,13 +1,18 @@
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
+from typing import Literal
 from src.models.lstm_video_classifier import LSTMVideoClassifier
+from src.models.transformer_video_classifier import TransformerVideoClassifier
 
 class VideoClassificationTask(pl.LightningModule):
-    def __init__(self, learning_rate=0.001, **model_config):
+    def __init__(self, learning_rate=0.001, temoral_modeling: Literal["lstm", "transformer"] = "lstm", **model_config):
         super().__init__()
         self.save_hyperparameters()
-        self.model = LSTMVideoClassifier(**model_config)
+        if temoral_modeling == "lstm":
+            self.model = LSTMVideoClassifier(**model_config)
+        elif temoral_modeling == "transformer":
+            self.model = TransformerVideoClassifier(**model_config)
         self.learning_rate = learning_rate
 
     def training_step(self, batch, batch_idx):
@@ -27,8 +32,9 @@ class VideoClassificationTask(pl.LightningModule):
 
     def predict_step(self, batch, batch_idx):
         image = batch["video"]
-        video_class = self.model(image)
-        return video_class
+        logits = self.model(image)
+        video_class = torch.argmax(logits, dim=1)
+        return {"class": video_class}
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.learning_rate)
