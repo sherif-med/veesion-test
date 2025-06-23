d3bb49b1a2714b279fe3cab994c4649bb56f2747
sherif-med <35470250+sherif-med@users.noreply.github.com>
2025-06-23 02:36:27 +0200

    updating sslFrame task (using another loss impl)

--- src/tasks/ssl_frame.py
+++ src/tasks/ssl_frame.py
@@ -1,25 +1,80 @@
 import torch
-import torch.nn as nn
 import pytorch_lightning as pl
 from src.models.frame_encoder import FrameEncoder
 
-def nt_xent_loss(z1, z2, temperature=0.5):
-    z1 = nn.functional.normalize(z1, dim=1)
-    z2 = nn.functional.normalize(z2, dim=1)
-    representations = torch.cat([z1, z2], dim=0)
-    similarity = torch.mm(representations, representations.t())
-    labels = torch.arange(z1.size(0)).repeat(2).to(z1.device)
+import torch
+import numpy as np
 
-    logits = similarity / temperature
-    logits = logits - torch.eye(logits.size(0), device=logits.device) * 1e9  # mask self-similarity
-    return nn.CrossEntropyLoss()(logits, labels)
+class NTXentLoss(torch.nn.Module):
+
+    def __init__(self, device, batch_size, temperature, use_cosine_similarity):
+        super(NTXentLoss, self).__init__()
+        self.batch_size = batch_size
+        self.temperature = temperature
+        self.device = device
+        self.softmax = torch.nn.Softmax(dim=-1)
+        self.mask_samples_from_same_repr = self._get_correlated_mask().type(torch.bool)
+        self.similarity_function = self._get_similarity_function(use_cosine_similarity)
+        self.criterion = torch.nn.CrossEntropyLoss(reduction="sum")
+
+    def _get_similarity_function(self, use_cosine_similarity):
+        if use_cosine_similarity:
+            self._cosine_similarity = torch.nn.CosineSimilarity(dim=-1)
+            return self._cosine_simililarity
+        else:
+            return self._dot_simililarity
+    def _get_correlated_mask(self):
+        diag = np.eye(2 * self.batch_size)
+        l1 = np.eye((2 * self.batch_size), 2 * self.batch_size, k=-self.batch_size)
+        l2 = np.eye((2 * self.batch_size), 2 * self.batch_size, k=self.batch_size)
+        mask = torch.from_numpy((diag + l1 + l2))
+        mask = (1 - mask).type(torch.bool)
+        return mask.to(self.device)
+
+    @staticmethod
+    def _dot_simililarity(x, y):
+        v = torch.tensordot(x.unsqueeze(1), y.T.unsqueeze(0), dims=2)
+        # x shape: (N, 1, C)
+        # y shape: (1, C, 2N)
+        # v shape: (N, 2N)
+        return v
+
+    def _cosine_simililarity(self, x, y):
+        # x shape: (N, 1, C)
+        # y shape: (1, 2N, C)
+        # v shape: (N, 2N)
+        v = self._cosine_similarity(x.unsqueeze(1), y.unsqueeze(0))
+        return v
+
+    def forward(self, zis, zjs):
+        zis = torch.nn.functional.normalize(zis, dim=1)
+        zjs = torch.nn.functional.normalize(zjs, dim=1)
+        representations = torch.cat([zjs, zis], dim=0)
+
+        similarity_matrix = self.similarity_function(representations, representations)
+
+        # filter out the scores from the positive samples
+        l_pos = torch.diag(similarity_matrix, self.batch_size)
+        r_pos = torch.diag(similarity_matrix, -self.batch_size)
+        positives = torch.cat([l_pos, r_pos]).view(2 * self.batch_size, 1)
+
+        negatives = similarity_matrix[self.mask_samples_from_same_repr].view(2 * self.batch_size, -1)
+
+        logits = torch.cat((positives, negatives), dim=1)
+        logits /= self.temperature
+
+        labels = torch.zeros(2 * self.batch_size).to(self.device).long().to(logits.device)
+        loss = self.criterion(logits, labels)
+
+        return loss / (2 * self.batch_size)
 
 
 class SSLFrameTask(pl.LightningModule):
-    def __init__(self, learning_rate=0.001, **model_config):
+    def __init__(self, batch_size, temperature=0.5, use_cosine_similarity=True, learning_rate=0.001,**model_config):
         super().__init__()
         self.save_hyperparameters()
         self.model : torch.nn.Module = FrameEncoder(**model_config)
+        self.loss = NTXentLoss(self.device, batch_size, temperature, use_cosine_similarity)
         self.learning_rate = learning_rate
 
 
@@ -28,8 +83,8 @@ class SSLFrameTask(pl.LightningModule):
         pair_right = batch["pair_right"]
         z1 = self.model(pair_left)
         z2 = self.model(pair_right)
-        loss = nt_xent_loss(z1, z2)
-        self.log('ssl_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        loss = self.loss(z1, z2)
+        self.log('ssl_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
 
